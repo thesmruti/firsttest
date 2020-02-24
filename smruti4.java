4df286a67d1502f7cf952c204043a80491e4ef00
thesmruti <41378755+thesmruti@users.noreply.github.com>
2020-02-24 21:26:29 +0530

    Add files via upload

---
+++ smruti4.java
@@ -0,0 +1,12 @@
+public class smruti4{
+public static void main(String args[]){
+
+int a=20;
+int b=-10;
+boolean c=true;
+boolean d=false;
+System.out.println(~a);
+System.out.println(~b);
+System.out.println(!c);
+System.out.println(!d);
+}}
